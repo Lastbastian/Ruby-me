281a54d71f7f50a87c71f3f60f796c0320df7746
Chris Bastian <peacethrubeats@gmail.com>
2015-11-26 10:54:09 -0800

    udt

--- Week_13/shape.rb
+++ Week_13/shape.rb
@@ -13,25 +13,63 @@ class Shape
   end
 
   def to_s
-    "Shape class with corrdinates: #{@x} x #{@y}"
+    self.class.to_s + "#{@x} x #{@y}"
+  end
+
+  def create_shape(shape)
+    puts "Hey"
+
   end
 
   class Circle < Shape
+    attr_accessor :radius
+
+    def initialize(radius, x, y)
+      @radius = radius
+    end
+
     def area
-      area = PI * radius**2
+      area = PI_CONST * radius**2
+    end
+
+    def to_s
+      super(x,y)
     end
 
   end
 
   class Rectangle < Shape
+    attr_accessor :length, :width
+
+    def initialize(length, width)
+      @length = length
+      @width = width
+    end
+
     def area
       area = length * width
+    end
+
+    def to_s
+      super(x,y)
     end
   end
 
   class Triangle < Shape
+    attr_accessor :height, :base
+
+    def initialize(height, base)
+      @height = height
+      @base = base
+    end
+
     def area
       area = (height * base) / 2
+    end
+
+    def to_s
+
+      super(x,y)
     end
 
   end
@@ -42,3 +80,7 @@ end
 square = Shape.new(5,5)
 p square.to_s
 p square.location
+
+triangle = Shape.create_shape(:triangle)
+
+# s = Shape.new(1,1)
