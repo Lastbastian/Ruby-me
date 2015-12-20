a3490c0fd8c047de8f94288dc9eb8a932476e195
Chris Bastian <peacethrubeats@gmail.com>
2015-12-19 17:54:20 -0800

    rdoc update

---
+++ Week_17/rdoc_ex.rb
@@ -0,0 +1,12 @@
+#= MY RDOC EXAMPLE
+#
+#== This is a heading
+#
+#* First item in an outer list
+#  * First item in an innter list
+#  * Second item in an inner list
+#* Second item in an outer list
+#  * Only item in this inner list
+#
+#== This is a second heading
+#
